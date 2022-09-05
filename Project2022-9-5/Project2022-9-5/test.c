ef5085a76d903a4b6294f867259bad6317d43a46
华仔 <108916044+Aiyowei-huazai@users.noreply.github.com>
2022-09-05 12:08:24 +0800

    Add files via upload

---
+++ Project2022-9-5/Project2022-9-5/test.c
@@ -0,0 +1,33 @@
+//
+
+
+
+
+#include<stdio.h>
+
+
+
+int main()
+{
+	/*int x = 0, y = 0;
+	for (y = 1, x = 1; y <= 50; y++)
+	{
+		if (x >= 10) break;
+		if (x % 2 == 1)
+		{
+			x += 5;
+			continue;
+		}
+		x -= 3;
+		
+	}
+
+	printf("%d", y);*/
+
+
+	int a[2][3] = { {1},{2},{3} };
+	//int a[][3] = { 1,2,3,4,5,6 };
+
+
+	return 0;
+}
