9a6004da92b52d8137a28837d6ef29d50b6f1e2c
华仔 <108916044+Aiyowei-huazai@users.noreply.github.com>
2022-09-08 11:13:43 +0800

    Add files via upload

---
+++ Project2022-9-8/Project2022-9-8/源.cpp
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<string.h>
+
+
+//int main()
+//{
+//	double t=0;
+//	//printf("%lf\n", t = 2, t + 5, t++);
+//	printf("%lf\n", (t = 2, t + 5, t++));
+//	
+//	//printf("%lf\n", t++);
+//	printf("%lf\n", t);
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	////printf("%d", strlen("abcd\n00a\0\\"));
+//	//int a=0, b=0, k=0;
+//	//k = a > b;
+//	int a = 0;
+//	(a = 3 * 5, a * 4);
+//	printf("%d\n", a + 15);
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	///*int a = 1234;
+//	//printf("%6d\n", a);*/
+//
+//	//double pi = 3.14159;
+//	//printf("%5.2lf",pi);
+//
+//	//int a = 3, b = 5, x, y;
+//	//x = a + 1, ++b;
+//	//y = (a + 1, b + 6);
+//	//printf("x=%d,y=%d\n", x, y);
+//	//printf("b=%d", b);
+//
+//
+//	//int a = 3, b = 2;
+//
+//
+//	//printf("%d", (--a != b++) ? --a : ++b);
+//
+//	//int x=0, a=0, b=0, c=0;
+//
+//
+//	//if (a >< b)x++;
+//
+//	int a, b = 1, c = 2;
+//	a = b + c, a + b, c + 3;
+//	c = (c) ? a++ : b--;
+//
+//	printf("c=%d\n", (a + b, c));
+//	printf("%d", a);
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+int main()
+{
+	/*int x = 8;
+	if (x+1 > 8)
+	{
+		printf("%d\n", ++x);
+		printf("xixixi");
+	}
+	else
+	{
+		printf("%d\n", x--);
+		printf("hhhh");
+	}*/
+
+
+	//int a, b;
+	//scanf("%d,%d", &a, &b);
+	//if (a > b)a = b; //b = a;
+	//else a++; b++;
+	//printf("%d,%d", a, b);
+
+	//int s, i;
+	//for (s = 0, i = 1; i < 3; i++, s += i);
+	//printf("%d\n", s);
+	//
+
+
+
+
+	/*int y, x;
+	for (y = 1; y < 10;)y = ((x = y * 3, x + 1), x - 1);
+	printf("x=%d,y=%d", x, y);*/
+
+
+	int k, i, j;
+	for (i = 0; i <= 2; i++)
+	{
+		for (k = 1; k <= i; k++) printf(" ");
+		for (j = 0; j <= 3; j++) printf("*");
+		printf("\n");
+	}
+
+
+	return 0;
+}
